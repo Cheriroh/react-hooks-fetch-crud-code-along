20a0474eca6cca8573ec591f55b643e4e07f62d9
Cheriroh <prudencec2019@gmail.com>
2025-01-24 22:21:59 +0300

    phase-2/react-hooks-fetch-crud-code-along

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,37 +1,46 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import PropTypes from "prop-types";
 
-function ItemForm() {
+const ItemForm = ({ onAddItem }) => {
   const [name, setName] = useState("");
-  const [category, setCategory] = useState("Produce");
+  const [category, setCategory] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name || !category) return;
+
+    onAddItem({ name, category });
+    setName("");
+    setCategory("");
+  };
 
   return (
-    <form className="NewItem">
-      <label>
-        Name:
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
-
-      <label>
-        Category:
-        <select
-          name="category"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        >
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </label>
-
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        id="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required
+      />
+     
+      <label htmlFor="category">Category</label>
+      <input
+        type="text"
+        id="category"
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+        required
+      />
+     
       <button type="submit">Add to List</button>
     </form>
   );
-}
+};
+
+ItemForm.propTypes = {
+  onAddItem: PropTypes.func.isRequired,
+};
 
 export default ItemForm;
